f3374ea70731da9aef990a24532ecca717b11be7
Grace Liu <44302181+graceliu1@users.noreply.github.com>
2021-10-30 18:43:01 -0700

    Update technology.js

--- src/pages/technology.js
+++ src/pages/technology.js
@@ -31,7 +31,7 @@ const ThirdPage = () => (
        <p className = {sectionName}>Code Garden</p>
   
 
-  <img  className = {websiteImage} src = {website1_math}/>
+  <img style = {{margin: '10px 15px 10px 15px', boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)', maxHeight: '300px'}} className = {websiteImage} src = {website1_math}/>
   <img style = {{margin: '10px 15px 10px 15px', boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)', maxHeight: '300px'}} className = {websiteImage} src = {website2_eluo}/>
   <img style = {{margin: '10px 15px 10px 15px', boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)', maxHeight: '300px'}} className = {websiteImage} src = {website3_gma}/>
   <img style = {{margin: '10px 15px 10px 15px', boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)', maxHeight: '300px'}} className = {websiteImage} src = {website4_classify}/>
@@ -39,7 +39,7 @@ const ThirdPage = () => (
 
       
     <Section sectionTitle = "Robotics" style = {{marginTop: '20px', minWidth: '80%'}}>
-         <div className = {sectionRobotics} style = {{backgroundColor: 'rgb(120, 235, 177)'}}>
+         <div className = {sectionRobotics} style = {{backgroundColor: 'rgb(120, 235, 177)', minWidth: '50%'}}>
           
           <div className = {sectionContent} >
             <p className = {sectionName}>Robotic Field</p>
@@ -53,10 +53,10 @@ const ThirdPage = () => (
 
           </div>
       </Section>
-    <img style = {{margin: '10px 10px 10px 10px', width: '150px'}} src = {robot1}/>
-      <img style = {{margin: '10px 10px 10px 10px', width: '150px'}} src = {robot2}/>
-        <img style = {{margin: '10px 10px 10px 10px', width: '150px'}} src = {robot3}/>
-          <img style = {{margin: '10px 10px 10px 10px', width: '150px'}} src = {robot4}/>
+    <img style = {{margin: '10px 10px 10px 10px', width: '200px'}} src = {robot1}/>
+    <img style = {{margin: '10px 10px 10px 10px', width: '200px'}} src = {robot2}/>
+    <img style = {{margin: '10px 10px 10px 10px', width: '200px'}} src = {robot3}/>
+    <img style = {{margin: '10px 10px 10px 10px', width: '200px'}} src = {robot4}/>
       
 
 
