30c5a1e3a24e6bc6effdb46e26f4f4945c0b3107
Grace Liu <44302181+graceliu1@users.noreply.github.com>
2021-10-30 17:20:46 -0700

    Update technology.js

--- src/pages/technology.js
+++ src/pages/technology.js
@@ -14,50 +14,33 @@ import Section from '../components/section'
 import Button from '../components/button'
 import Seo from "../components/seo"
 
-import website1_math from "../images/website1-math.png"
-import website2_eluo from "../images/website2-eluo.png"
-import website3_gma from "../images/website3-gma.png"
-import website4_classify from "../images/website4-classify.png"
+import website1_math from "../images/website1-math.PNG"
+import website2_eluo from "../images/website2-eluo.PNG"
+import website3_gma from "../images/website3-gma.PNG"
+import website4_classify from "../images/website4-classify.PNG"
 
 const ThirdPage = () => (
   <Layout>
     <Seo title="Technology" />
-     
+       <p className = {sectionName}Code Garden ---------------------------------------------------------- </p>
             
       <img style = {{boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)'}} className = {websiteImage} src = {website1_math}/>
       <img style = {{boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)'}} className = {websiteImage} src = {website2_eluo}/>
       <img style = {{boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)'}} className = {websiteImage} src = {website3_gma}/>
       <img style = {{boxShadow: ' 0px 2px 5px rgba(155, 155, 155, 0.8)'}} className = {websiteImage} src = {website4_classify}/>
-              
-   
-      <Section sectionTitle = "Short Biography" style = {{marginTop: '20px'}}>
-         <div className = {sectionAboutMe}
-         style = {{backgroundColor: 'rgb(250, 209, 198)'}}
-      >
-          
-          <div className = {sectionContent} >
-            <p className = {sectionName}>Full-Stack Projects</p>
-            
-            <p style = {{lineHeight: '1.5'}}>
-            Text 1
-            </p>
+      
+      <p className = {sectionName} ----------------------------------------------------- Robot Field </p>
 
-            <Link to="/"><Button linkDescription = "go back to the homepage" color = "greenButton"></Button></Link>
-          </div>
-
-          </div>
-      </Section>
 
     <Section sectionTitle = "Robotics" style = {{marginTop: '20px'}}>
          <div className = {sectionAboutMe}
-         style = {{backgroundColor: 'rgb(250, 209, 198)'}}
-      >
+         style = {{backgroundColor: 'rgb(120, 235, 177)'}}>
           
           <div className = {sectionContent} >
-            <p className = {sectionName}>Robotics</p>
+            <p className = {sectionName}>Robotic Field</p>
             
-            <p style = {{lineHeight: '1.5'}}>
-            Text 1
+            <p>
+            I'm a member of our schoool robotics club. 
             </p>
 
             <Link to="/"><Button linkDescription = "go back to the homepage" color = "greenButton"></Button></Link>
