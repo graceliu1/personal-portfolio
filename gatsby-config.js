20c1e6696ff9ad149f37e9e05bcd5259a6de75ff
Grace Liu <44302181+graceliu1@users.noreply.github.com>
2021-11-03 23:10:13 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Grace Liu`,
+    description: `Grace Liu's personal portfolio.`,
+    author: `@graceliu1`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
